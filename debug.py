273990f5a93b7b3e3d57397191d4120438602e9d
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-03-19 15:11:48 +0000

    changes

--- debug.py
+++ debug.py
@@ -1,5 +1,14 @@
 import os
+import sys
+if len(sys.argv) < 2:
+    print("❌ Error: No YAML file path provided!")
+    sys.exit(1)
 
+config_path = sys.argv[1]  # Get the file path from GitHub Actions input
+
+if not os.path.isfile(config_path):
+    print(f"❌ Error: Config file '{config_path}' not found!")
+    sys.exit(1)
 paths = [
     "/usr/src/app",
     "/github/workspace",
